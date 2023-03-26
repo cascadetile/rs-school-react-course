b9b7cbf9908ea216b02cb23960b6685d3b155709
cascadetile <collent.credev@gmail.com>
2023-03-26 18:03:36 +0600

    feat: disable Form after submit

--- src/pages/People/Form/index.tsx
+++ src/pages/People/Form/index.tsx
@@ -11,6 +11,7 @@ interface IFormProps {
   maleRef: React.RefObject<HTMLInputElement>
   femaleRef: React.RefObject<HTMLInputElement>
   fileRef: React.RefObject<HTMLInputElement>
+  fieldsetRef: React.RefObject<HTMLFieldSetElement>
   errors: IFormValidatorState
 }
 
@@ -22,55 +23,57 @@ export class FormPeople extends React.Component<IFormProps, Record<string, never
     return (
       <>
         <form onSubmit={this.props.handleSubmit} className="form">
-          <label htmlFor="name">
-            Name:
-          </label>
-          <input id="name" ref={this.props.nameRef} type="text" placeholder="Name" />
-          {
-            this.props.errors.errorName && <p>{this.props.errors.errorName}</p>
-          }
-          <label htmlFor="date">Birthday:</label>
-          <input id="date" ref={this.props.dateRef} type="date" />
-          {
-            this.props.errors.errorDate && <p>{this.props.errors.errorDate}</p>
-          }
-          <label htmlFor="country">Select your country:</label>
-          <select id="country" ref={this.props.countryRef} defaultValue="default">
-            <option disabled value="default">Select your country</option>
-            <option value="BY">BY</option>
-            <option value="RU">RU</option>
-            <option value="KZ">KZ</option>
-          </select>
-          {
-            this.props.errors.errorCountry && <p>{this.props.errors.errorCountry}</p>
-          }
-          <label>
-            <input ref={this.props.agreeRef} type="checkbox" />
-            I agree with agreement
-          </label>
-          {
-            this.props.errors.errorAgree && <p>{this.props.errors.errorAgree}</p>
-          }
-          <div>
-            Choose your gender:
-            <label>
-              <input ref={this.props.maleRef} name="sex" type="radio" value="Male" />
-              Male
+          <fieldset ref={this.props.fieldsetRef}>
+            <label htmlFor="name">
+              Name:
             </label>
+            <input id="name" ref={this.props.nameRef} type="text" placeholder="Name" />
+            {
+              this.props.errors.errorName && <p>{this.props.errors.errorName}</p>
+            }
+            <label htmlFor="date">Birthday:</label>
+            <input id="date" ref={this.props.dateRef} type="date" />
+            {
+              this.props.errors.errorDate && <p>{this.props.errors.errorDate}</p>
+            }
+            <label htmlFor="country">Select your country:</label>
+            <select id="country" ref={this.props.countryRef} defaultValue="default">
+              <option disabled value="default">Select your country</option>
+              <option value="BY">BY</option>
+              <option value="RU">RU</option>
+              <option value="KZ">KZ</option>
+            </select>
+            {
+              this.props.errors.errorCountry && <p>{this.props.errors.errorCountry}</p>
+            }
             <label>
-              <input ref={this.props.femaleRef} name="sex" type="radio" value="Female" />
-              Female
+              <input ref={this.props.agreeRef} type="checkbox" />
+              I agree with agreement
             </label>
-          </div>
-          {
-            this.props.errors.errorGender && <p>{this.props.errors.errorGender}</p>
-          }
-          <label htmlFor="pfp">Choose your profile picture:</label>
-          <input id="pfp" ref={this.props.fileRef} type="file" />
-          {
-            this.props.errors.errorFile && <p>{this.props.errors.errorFile}</p>
-          } 
-          <input type="submit" value="Send" />
+            {
+              this.props.errors.errorAgree && <p>{this.props.errors.errorAgree}</p>
+            }
+            <div>
+              Choose your gender:
+              <label>
+                <input ref={this.props.maleRef} name="sex" type="radio" value="Male" />
+                Male
+              </label>
+              <label>
+                <input ref={this.props.femaleRef} name="sex" type="radio" value="Female" />
+                Female
+              </label>
+            </div>
+            {
+              this.props.errors.errorGender && <p>{this.props.errors.errorGender}</p>
+            }
+            <label htmlFor="pfp">Choose your profile picture:</label>
+            <input id="pfp" ref={this.props.fileRef} type="file" />
+            {
+              this.props.errors.errorFile && <p>{this.props.errors.errorFile}</p>
+            } 
+            <input type="submit" value="Send" />
+          </fieldset>
         </form>
       </>
     )
--- src/pages/People/FormValidator/index.tsx
+++ src/pages/People/FormValidator/index.tsx
@@ -14,6 +14,7 @@ export class FormValidator extends React.Component<IProps, IFormValidatorState> 
   male: React.RefObject<HTMLInputElement>;
   female: React.RefObject<HTMLInputElement>;
   file: React.RefObject<HTMLInputElement>;
+  fieldset: React.RefObject<HTMLFieldSetElement>;
   constructor(props: IProps) {
     super(props);
     this.name = React.createRef();
@@ -23,6 +24,7 @@ export class FormValidator extends React.Component<IProps, IFormValidatorState> 
     this.male = React.createRef();
     this.female = React.createRef();
     this.file = React.createRef();
+    this.fieldset = React.createRef();
     this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
       errorName: '',
@@ -47,7 +49,7 @@ export class FormValidator extends React.Component<IProps, IFormValidatorState> 
     if (this.name.current && this.date.current &&
       this.country.current && this.agree.current &&
       this.male.current && this.female.current &&
-      this.file.current) {
+      this.file.current && this.fieldset.current) {
       let valid = true;
       if (!this.name.current.value) {
         newState.errorName = 'Name should not be empty';
@@ -85,13 +87,33 @@ export class FormValidator extends React.Component<IProps, IFormValidatorState> 
           gender: this.getGenderValue(),
           file: this.file.current.files?.length ? this.file.current.files[0] : '',
         });
-        this.name.current.value = '';
-        this.date.current.value = '';
-        this.country.current.value = 'default';
-        this.agree.current.checked = false;
-        this.male.current.checked = false;
-        this.female.current.checked = false;
-        this.file.current.value = '';
+        this.fieldset.current.disabled = true;
+        setTimeout(() => {
+          if (this.fieldset.current) {
+            this.fieldset.current.disabled = false;
+          }
+          if (this.name.current) {
+            this.name.current.value = '';
+          }
+          if (this.date.current) {
+            this.date.current.value = '';
+          }
+          if (this.country.current) {
+            this.country.current.value = 'default';
+          }
+          if (this.agree.current) {
+            this.agree.current.checked = false;
+          }
+          if (this.male.current) {
+            this.male.current.checked = false;
+          }
+          if (this.female.current) {
+            this.female.current.checked = false;
+          }
+          if (this.file.current) {
+            this.file.current.value = '';
+          }
+        }, 3000);
       }
     }
     this.setState(newState);
@@ -121,6 +143,7 @@ export class FormValidator extends React.Component<IProps, IFormValidatorState> 
           maleRef={this.male}
           femaleRef={this.female}
           fileRef={this.file}
+          fieldsetRef={this.fieldset}
           errors={this.state}
         />
       </>
