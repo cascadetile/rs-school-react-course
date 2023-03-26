fa4f24fe61a3abdb0f8c9622047bbd0c0f797592
cascadetile <collent.credev@gmail.com>
2023-03-26 16:28:45 +0600

    feat: change Card from props to slots

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import './style.css';
 
 interface IProps {
-  imgUrl: string
-  title: string
-  description: string
+  children: ReactNode
 }
 
 export class Card extends React.Component<IProps> {
@@ -14,11 +12,7 @@ export class Card extends React.Component<IProps> {
   render(): React.ReactNode {
     return (
       <div className="card">
-        <div className="card__img-wrapper">
-          <img className="card__img" src={this.props.imgUrl} alt="" />
-        </div>
-        <h3 className="card__title">{this.props.title}</h3>
-        <p className="card__description">{this.props.description}</p>
+        {this.props.children}
       </div>
     );
   }
